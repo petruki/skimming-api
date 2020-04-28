638da1be7639bb37e786b7c8d347c7cd798bad6e
petruki <31597636+petruki@users.noreply.github.com>
2020-04-27 20:51:01 -0700

    Added endpoints to arc

--- src/http/get-config/index.ts
+++ src/http/get-config/index.ts
@@ -7,12 +7,12 @@ export async function handler(req: object) {
       "content-type": "application/json; charset=utf8",
       "cache-control": "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
     },
-    body: {
+    body: JSON.stringify({
         message: 'Success',
         url: APP_CONTEXT_ENDPOINT,
         files: APP_FILES.split(","),
         cacheDuration: `${APP_CACHE_EXP_DURATION}s`,
         cacheSize: APP_CACHE_SIZE
-    }
+    })
   };
 }
