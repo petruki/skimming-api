3a8cb09eac26558d9b564e8b0d0ca4829fc50c1b
petruki <31597636+petruki@users.noreply.github.com>
2020-05-07 00:28:59 -0700

    Updated Skimming module to v1.0.5

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,12 @@
-import { config } from "https://deno.land/x/dotenv/dotenv.ts";
+const env = Deno.env;
 
-export const APP_HOST = config().APP_HOST;
-export const APP_PORT = config().APP_PORT;
-export const APP_CONTEXT_ENDPOINT = config().APP_CONTEXT_ENDPOINT;
-export const APP_FILES = config().APP_FILES;
-export const APP_CACHE_EXP_DURATION: number = parseInt(config().APP_CACHE_EXP_DURATION);
-export const APP_CACHE_SIZE: number = parseInt(config().APP_CACHE_SIZE);
+export const APP_HOST = env.get('APP_HOST') || "127.0.0.1";
+export const APP_PORT = env.get('APP_PORT') || 4000;
+export const APP_CONTEXT_ENDPOINT = env.get('APP_CONTEXT_ENDPOINT') || "https://raw.githubusercontent.com/petruki/skimming/master/";
+export const APP_FILES = env.get('APP_FILES') || "README.md";
+
+const ENV_APP_CACHE_EXP_DURATION = env.get('APP_CACHE_EXP_DURATION') || '';
+export const APP_CACHE_EXP_DURATION: number = parseInt(ENV_APP_CACHE_EXP_DURATION) || 5;
+
+const ENV_APP_CACHE_SIZE = env.get('APP_CACHE_SIZE') || '';
+export const APP_CACHE_SIZE: number = parseInt(ENV_APP_CACHE_SIZE) || 100; 
